fc9e4a82c2bbb49dfa1b52f5df411ac670f7c3b1
manishsaraan <manish021js@gmail.com>
2018-07-03 12:39:50 +0530

    Revert "WIP: http-helper utility"
    This reverts commit f46dcc5f0f4dbb08bd855ab3a633aa673d8a5741.

--- src/sagas/book.js
+++ src/sagas/book.js
@@ -1,9 +1,7 @@
-import { put, call, takeEvery } from 'redux-saga/effects';
+import { put, takeEvery } from 'redux-saga/effects';
 import { TEST, SEND_TEST } from "../actions/book";
-import getRequest from '../utils/http-helper';
 
 export function* getBooks() {
-    yield call(getRequest, { url: 'search/index.xml?q=rich'});
     yield put({ type: SEND_TEST});
 }
 
